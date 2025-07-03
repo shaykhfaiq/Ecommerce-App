e2f3be65f879822f5ca8846843406e7224d346f0
Mac <mac@174-16-0-41.hlrn.qwest.net>
2025-07-03 17:42:09 +0500

    Dashboard Side Bar and logout Functionality Implemneted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :seller_auth_controller?, :seller_user?
+
+  def seller_auth_controller?
+    controller_path.start_with?("sellers_auth/")
+  end
+
+  def seller_user?
+    current_user&.has_role?(:seller)
+  end
 end
--- app/controllers/dashboard/seller_controller.rb
+++ app/controllers/dashboard/seller_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/dashboard/seller_controller.rb
 module Dashboard
   class SellerController < ApplicationController
+    layout "seller_layout"
     def index
       # Seller dashboard logic here
     end
